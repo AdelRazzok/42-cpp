61892da830b3344351bd5576c6574d55c2d486ed
AdelRazzok <lhp6.arazzok@gmail.com>
2024-03-19 12:45:38 +0100

    classes

---
+++ cpp-00/ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 21:00:07 by arazzok           #+#    #+#             */
+/*   Updated: 2024/02/20 21:35:48 by arazzok          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_H
+# define PHONE_BOOK_H
+
+# include "Contact.hpp"
+
+class PhoneBook {
+    private:
+        Contact contacts[8];
+};
+
+#endif
