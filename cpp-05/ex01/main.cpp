e16e3f68231a699fb577fdb328f4ffc2c22fa621
Adel Razzok <arazzok@c2r9p5.42lehavre.fr>
2024-05-16 17:38:51 +0200

    test

--- cpp-05/ex01/main.cpp
+++ cpp-05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 14:49:13 by arazzok           #+#    #+#             */
-/*   Updated: 2024/05/16 17:23:11 by arazzok          ###   ########.fr       */
+/*   Updated: 2024/05/16 17:38:31 by arazzok          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,5 @@ int main(void)
     std::cout << std::endl;    
     std::cout << b12 << std::endl;
 
-    return 0;
+    return 0; 
 }
